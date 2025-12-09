f093476a0966e9202fb1fb092d9f0e0d15db8c10
mportillo <marco.portillo@gmail.com>
2025-12-09 01:11:35 +0100

    partially done part 1 challenge day 1 and added input file

--- Day 1/SecretEntrance.java
+++ Day 1/SecretEntrance.java
@@ -1,5 +1,61 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class SecretEntrance {
     public static void main(String[] args) {
-        System.out.println("Hello world 1");
+        ArrayList<String> fileData = readFile("puzzleInput.in");
+        if(fileData.size() > 0) {
+            int dialNumber = 50;
+            int passwordNumber = 0;
+            for (String StringNumber : fileData) {
+                int IntegerNumber = extractCurrentNumberAsInteger(StringNumber);
+                
+                if(StringNumber.startsWith("L")) {
+                    dialNumber = dialNumber - IntegerNumber;
+                    if(dialNumber < 0) {
+                        dialNumber = 100 - Math.abs(dialNumber);
+                    }
+                    if(dialNumber == 0) {
+                        passwordNumber++;
+                    } 
+                    
+                }
+                if(StringNumber.startsWith("R")) {
+                    dialNumber = dialNumber + IntegerNumber;
+                    if(dialNumber > 99) {
+                        dialNumber = 0 + (dialNumber - 100);
+                    }
+                    if(dialNumber == 0) {
+                        passwordNumber++;
+                    }
+                }
+            }
+            System.out.println(passwordNumber);
+        } else {
+            System.out.println("File has not data");
+        }
     }
+
+    public static Integer extractCurrentNumberAsInteger(String number) {
+        return Integer.parseInt(number.substring(1));
+    }
+
+    public static ArrayList<String> readFile(String fileName) {
+        File puzzleInput = new File(fileName);
+        ArrayList<String> data = new ArrayList<String>();
+        try (Scanner myReader = new Scanner(puzzleInput)) {
+            while (myReader.hasNextLine()) {
+                 data.add(myReader.nextLine());
+                 System.out.println(data);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found Exception");
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+    
 }
