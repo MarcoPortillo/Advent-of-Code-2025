592ca5b7ab3891d1ce44bc9c7c376be86e0eb932
mportillo <marco.portillo@gmail.com>
2025-12-15 20:32:13 +0100

    finished day 2 part 1, and started part 2

---
+++ Day 2/Part 1/GiftShop.java
@@ -0,0 +1,86 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class GiftShop {
+   
+        public static void main(String[] args) {
+            long totalInvalidadIDs = 0;
+            String[] rangesID = readFile("giftShopInput.in");
+            for(String range : rangesID) {
+                long start = Long.parseLong(range.split("-")[0]);
+                long end = Long.parseLong(range.split("-")[1]);
+                    for(long i=start;i<=end;i++) {
+                        String number = String.valueOf(i);
+                        int numberLength = number.length();
+
+                         if (numberLength % 2 != 0) {
+                            continue;
+                        }
+
+
+                        int half = numberLength / 2; 
+                        boolean isRepeated = false;
+
+                        int index = 0;
+                        while(index < half) { 
+
+                            String offset = number.substring(0, index + 1);
+
+                            if (offset.length() != numberLength / 2) {
+                                index++;
+                                continue;
+                            }
+
+
+                            int offsetLength = offset.length();
+                            int next = offsetLength;
+
+                            boolean valid = true;
+
+                                 while (next + offsetLength <= numberLength) {
+                                    String sequence = number.substring(next, next + offsetLength);
+
+                                    if (!offset.equals(sequence)) {
+                                        valid = false;
+                                        break;
+                                    }
+
+                                    next += offsetLength;
+                                }
+
+                            if (valid && next == numberLength) {
+                                    isRepeated = true;
+                                    break;
+                            }
+
+                            index++;
+                        }
+
+                        if (isRepeated) {
+                            totalInvalidadIDs += i;
+                        }
+                    }
+                }   
+            System.out.println(totalInvalidadIDs);
+        }
+
+
+        public static String[] readFile(String fileName) {
+            File puzzleInput = new File(fileName);
+            String [] data = null;
+            try (Scanner myReader = new Scanner(puzzleInput)) {
+                while (myReader.hasNextLine()) {
+                    String line = myReader.nextLine();
+                    data = line.split(",");
+                }
+            } catch (FileNotFoundException e) {
+                System.out.println("File not found Exception");
+                e.printStackTrace();
+            }
+            return data;
+        }
+
+}
+
+
